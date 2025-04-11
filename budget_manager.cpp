00a3930809ba2eac0e11469ceef9b37a61339345
ayaanazam01 <ranaayaanazam2005@gmail.com>
2025-04-11 09:09:49 -0700

    Update budget_manager.cpp

--- budget_manager.cpp
+++ budget_manager.cpp
@@ -1,1 +1,12 @@
+#include <iostream> // For input/output
+using namespace std;
+
+string PriorityNames[5]; // Array declared for quick access of Priority Names
+int PriorityRank[5];     // Array declared for accessing Priority Ranks
+
+struct Stack_Node
+{
+    int priorityrank_stack;
+    Stack_Node *next;
+};
 
