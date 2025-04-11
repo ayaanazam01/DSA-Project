7f6b992ffb1948d765b5a2e9b2488f387749812c
ayaanazam01 <ranaayaanazam2005@gmail.com>
2025-04-11 09:24:36 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "Plagiarism_detector.h"
+#include "budget_manager.h"
+#include "Time_Manager_Main.h>
+
+using namespace std;
+
+void displayMainMenu() {
+    cout << "Student Helper Program\n";
+    cout << "1. Budget Manager\n";
+    cout << "2. Plagiarism Detector\n";
+    cout << "3. Time Manager\n";
+    cout << "4. Exit\n";
+    cout << "Enter your choice: ";
+}
+
+int main() {
+    int choice;
+    do {
+        displayMainMenu();
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                budgetMenu();  // Function from budget_manager_2023049.cpp
+                break;
+            case 2:
+                plagiarismMenu();  // Function from Plagiarism_detector.cpp
+                break;
+            case 3:
+                timeManagerMenu();  // Function from Time_Manager_Main.cpp
+                break;
+            case 4:
+                cout << "Exiting program. Goodbye!\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 4);
+
+    return 0;
+}
